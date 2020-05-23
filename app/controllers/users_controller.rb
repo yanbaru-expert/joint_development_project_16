5fc654c40e1070a60862d71bd595f2e6c0832a14
morita daiki <morita.daiki.works@gmail.com>
2020-05-23 15:40:44 +0900

    indexページ 作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,19 @@
 class UsersController < ApplicationController
 
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
 
   def create
     User.create(user_params)
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
 
