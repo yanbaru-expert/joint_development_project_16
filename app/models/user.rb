6b72e8bfda1706356d9eee4113aa426a942c3b6d
morita daiki <morita.daiki.works@gmail.com>
2020-05-26 17:31:24 +0900

    inclusionで年齢を範囲指定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 50 }
-  validates :age, presence: true, length: { maximum: 3}, numericality: { only_integer: true }
+  validates :age, presence: true, inclusion: { in: 0..150 }, numericality: { only_integer: true }
 end
