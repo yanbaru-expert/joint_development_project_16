4d4f8d787ef8e5ea55137d20767d99d197d2f783
morita daiki <morita.daiki.works@gmail.com>
2020-05-27 14:48:14 +0900

    create,destory,update html削除,redirect追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 
   def create
     User.create(user_params)
+    redirect_to users_path
   end
 
   def show
@@ -23,11 +24,13 @@ class UsersController < ApplicationController
   def update
     user = User.find(params[:id])
     user.update(user_params)
+    redirect_to users_path
   end
 
   def destroy
     user = User.find(params[:id])
     user.destroy
+    redirect_to users_path
   end
 
   private
