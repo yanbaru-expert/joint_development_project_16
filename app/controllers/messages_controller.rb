b4ac1e3e5d562489449c5e9506fe9a2562587ea7
nakaryo <nakaryo@nakaryonoMacBook-ea.local>
2020-05-27 11:28:58 +0900

    詳細表示の実装及び、一覧画面のカラムを１つに修正

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,10 @@ class MessagesController < ApplicationController
   def index
     @messages = Message.all
   end
+
+  def show
+    @message = Message.find(params[:id])
+  end
   
   def new
     @message = Message.new
