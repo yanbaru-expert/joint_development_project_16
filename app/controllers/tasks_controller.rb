da21ee437112a0a3a4ba39e90ac49938db5a8c5c
Ikko-T <skyblueboy32@gmail.com>
2020-06-17 09:05:49 +0900

    詳細表示機能実装（カラム2つ表示）

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,13 +3,17 @@ class TasksController < ApplicationController
     def index
         @tasks = Task.all
     end
-    
+
     def new
         @task = Task.new
     end
 
     def create
         Task.create(task_params)
+    end
+
+    def show
+        @task = Task.find(params[:id])
     end
 
     private
